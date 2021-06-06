b9e7d6a813294a003abb848c9c3c5b767279073f
Jezda1337 <megasrbin1@gmail.com>
2021-06-06 19:16:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "components/Header";
+
+const App = () => {
+    return (
+        <header>
+            <Header/>
+        </header>
+    )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import "styles/Header.scss";
+import {FaSearch, FaThList} from 'react-icons/fa';
+const Header = () => {
+    return (
+        <nav className="header">
+            <a className="header__logo" href="/">
+                <h1>YFM</h1>
+            </a>
+            <ul className="header__menu open">
+                <li className="header__item">
+                    <input className="header__search" type="search" placeholder='Quick Search'/>
+                    <FaSearch className="header__search-icon icon"/>
+                </li>
+                <li className="header__item">
+                    <a className="header__link" href="/">Home</a>
+                </li>
+
+                <li className="header__item">
+                    <a className="header__link" href="browser__movies">Browser Movies</a>
+                    <FaThList className="header__browser-icon icon" />
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
