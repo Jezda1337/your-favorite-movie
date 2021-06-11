5f39cc12cad1d59ef44546fc1ef99be890b7e63f
Jezda1337 <megasrbin1@gmail.com>
2021-06-11 14:30:52 +0200

    fix :bug:

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Header from "components/Header";
-import Card from 'components/Card';
-import Search from 'components/Search';
-import 'styles/components/App.scss';
+import Card from "components/Card";
+import Search from "components/Search";
+import "styles/components/App.scss";
 
 const App = () => {
-    let [isOpen, setOpen] = useState(false);
-    console.log(isOpen);
-    
-    return <>
-        <header>
-            <Header setOpen={setOpen} isOpen={isOpen}/>
-        </header>
-        <main className="main">
-            <Search isOpen={isOpen}/>
+  let [isOpen, setOpen] = useState(false);
+  console.log(isOpen);
 
-            <Card/>
-        </main>
+  return (
+    <>
+      <header>
+        <Header setOpen={setOpen} isOpen={isOpen} />
+      </header>
+      <main className="main">
+        <Search isOpen={isOpen} />
+
+        <Card />
+      </main>
     </>
-}
+  );
+};
 
-export default App
+export default App;
